4d395f6c70d5e9935aa047cae897ae6d76701c53
Ліна Ільяшенко <lina.iliashenko@MacBook-Air-Lina.local>
2023-11-15 21:59:48 +0100

    fix

--- calc.php
+++ calc.php
@@ -1,13 +1,10 @@
 <?php
 
-// Check if the required GET parameters are set
 if(isset($_GET['num1']) && isset($_GET['num2']) && isset($_GET['sign'])) {
-    // Retrieve the values from $_GET
     $num1 = $_GET['num1'];
     $num2 = $_GET['num2'];
     $sign = $_GET['sign'];
 
-    // Perform the operation based on the sign
     switch($sign) {
         case '+':
             $result = $num1 + $num2;
@@ -19,7 +16,6 @@ if(isset($_GET['num1']) && isset($_GET['num2']) && isset($_GET['sign'])) {
             $result = $num1 * $num2;
             break;
         case '/':
-            // Check for division by zero
             if($num2 == 0) {
                 echo "Error: Division by zero.";
                 exit;
@@ -31,7 +27,6 @@ if(isset($_GET['num1']) && isset($_GET['num2']) && isset($_GET['sign'])) {
             exit;
     }
 
-    // Output the result
     echo $result;
 } else {
     echo "Error";
